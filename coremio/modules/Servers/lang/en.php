88dd0398f0a3a9a58bcd269cab1a21b9334330ae
FibaCloud Team <128176967+fibacloud@users.noreply.github.com>
2024-03-05 17:21:40 +0300

    Create en.php

---
+++ coremio/modules/Servers/lang/en.php
@@ -0,0 +1,23 @@
+<?php
+    return [
+        'productname'           => "Product Select",
+        'productnamedesc'       => "Please select a product to be paired with this service.",
+        'promocodename'         => "Promo Code",
+        'promocodedesc'         => "Optional promo code. It is applied to orders sent via API.",
+        'change-password'       => 'Change Password',
+        'turn-back'             => 'Turn Back',
+        'restart'               => 'Restart',
+        'start'                 => 'Start',
+        'starting'              => 'Starting',
+        'stop'                  => 'Stop',
+        'stopping'              => 'Stopping',
+        'hardstop'              => 'Hard Stop',
+        'reboot'                => 'Reboot',
+        'rebuild'               => 'Rebuild',
+        'apply'                 => 'Apply',
+        'successful'            => 'Process Successfully Done',
+        'error'                 => 'Process Failed',
+        'rebuildconfirm'        => 'This will rebuild your server. All your existing data will be deleted. Do you want to continue?',
+        'rebuildsuccessful'     => "Reinstallation Processed Successfully. Please allow 5-10 minutes for the process to complete. Operating System to be Installed:",
+        'rebuilderoor'          => "An Error Encountered While Processing. Please Contact Support. Error Output:"
+    ];
