c417749e9744e4d594594c78d97e889d773ccedb
FibaCloud Team <128176967+fibacloud@users.noreply.github.com>
2024-03-05 17:20:55 +0300

    Create config.php

---
+++ coremio/modules/Servers/config.php
@@ -0,0 +1,11 @@
+<?php
+    return [
+        'type'                          => "virtualization",
+        'access-hash'                   => false,
+        'server-info-checker'           => false,
+        'server-info-port'              => false,
+        'configurable-option-params'    => [
+            'Products',
+            'OS' => 'Operating System',
+        ],
+    ];
