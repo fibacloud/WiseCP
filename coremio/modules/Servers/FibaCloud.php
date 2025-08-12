e17521b2ac97a6064bb0fb2e2df08f871b560ecc
FibaCloud Team <128176967+fibacloud@users.noreply.github.com>
2025-08-12 18:05:22 +0300

    Update FibaCloud.php

--- coremio/modules/Servers/FibaCloud.php
+++ coremio/modules/Servers/FibaCloud.php
@@ -196,7 +196,7 @@ public function create(array $order_options=[]) {
     }
     
     // Step 2: Send order via POST
-    $url = "https://cloud.fibacloud.com/api/order/instances/{$product_id}";
+    $url = "https://cloud.fibacloud.com/api/order/{$product_id}";
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_POST, true);
